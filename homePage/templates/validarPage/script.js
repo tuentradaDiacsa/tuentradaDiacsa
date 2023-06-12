efa84ac533516494fc4c8d174b6b2b602ab086b2
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-12 11:24:25 -0500

    Add files via upload

--- homePage/templates/validarPage/script.js
+++ homePage/templates/validarPage/script.js
@@ -17,10 +17,6 @@ function soloNumeros(event) {
 }
 
 function verificar() {
-    if (document.getElementById("ticket").value.trim().length !== 6 || document.getElementById("cip").value.trim().length !== 6) {
-        alert("El codigo de verificacion debe tener 6 caracteres.");
-        return false; // Evita que el formulario se envíe
-    }
     $.ajax({
         type: 'POST',
         url: '/administrar/',  // Reemplaza esto con la URL de tu vista de Django
@@ -31,10 +27,30 @@ function verificar() {
             'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
         },
         success: function (response) {
-            document.getElementById("resultado").innerText = response.estado.toString();
-            if (response.estado.toString() == "Ticket valido") document.getElementById("datosCompradorTexto").style.backgroundColor = "green";
-            if (response.estado.toString() == "Ticket no valido") document.getElementById("datosCompradorTexto").style.backgroundColor = "red";
-            if (response.estado.toString() == "Ticket en validacion") document.getElementById("datosCompradorTexto").style.backgroundColor = "orange";
+            document.getElementById("comprarBotonFinal").style.display = "none"
+            document.getElementById("comprarBotonFinal").style.visibility = "visible"
+            document.getElementById("datosCompradorTexto").style.visibility = "visible"
+            document.getElementById("datosCompradorTexto").style.display = "flex"
+            document.getElementById("notasAlFinal").style.display = "grid"
+
+            if (response.estado.toString() == "Ticket valido") {
+                document.getElementById("resultado").innerText = "Validado"
+                document.getElementById("datosCompradorTexto").style.backgroundColor = "##7643F6";
+                document.getElementById("notas").innerText = "Entrada valida para el concierto del Grupo 5 en Iquitos Domingo 25 de Junio de 2023 - 3:00 PM"
+                document.getElementById("notas2").innerText = ""
+            }
+            if (response.estado.toString() == "Ticket no valido") {
+                document.getElementById("resultado").innerText = "Entrada no valida"
+                document.getElementById("datosCompradorTexto").style.backgroundColor = "black";
+                document.getElementById("notas").innerText = "1. Puede que la entrada o apellido sea incorrecto."
+                document.getElementById("notas2").innerText = "2. Verifique que la fecha del evento este vigente."
+            }
+            if (response.estado.toString() == "Ticket en validacion") {
+                document.getElementById("resultado").innerText = "Entrada por validar"
+                document.getElementById("datosCompradorTexto").style.backgroundColor = "gray";
+                document.getElementById("notas").innerText = "Su entrada aun esta en proceso de verificacion de pago"
+                document.getElementById("notas2").innerText = ""
+            }
             console.log(response);
         },
         error: function (xhr, status, error) {
@@ -42,4 +58,11 @@ function verificar() {
             console.error('Error:', error);
         }
     });
+}
+function inicial() {
+    document.getElementById("comprarBotonFinal").style.display = "flex"
+    document.getElementById("comprarBotonFinal").style.visibility = "visible"
+    document.getElementById("datosCompradorTexto").style.visibility = "none"
+    document.getElementById("datosCompradorTexto").style.display = "flex"
+    document.getElementById("notasAlFinal").style.display = "none"
 }
