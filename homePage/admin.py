f8ebf7fb1cca4ce9cc425155abafaf0ff04b0d96
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-11 19:51:00 -0500

    Update admin.py

--- homePage/admin.py
+++ homePage/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import smsValidacionCelular, Tipos, Pagos, Tickets
+from .models import smsValidacionCelular, Tipos, Pagos, Tickets, boxesRestante1, boxesRestante2, boxesRestante3
 # Register your models here.
 admin.site.register(smsValidacionCelular)
 admin.site.register(Tipos)
 admin.site.register(Pagos)
 admin.site.register(Tickets)
+admin.site.register(boxesRestante1)
+admin.site.register(boxesRestante2)
+admin.site.register(boxesRestante3)
