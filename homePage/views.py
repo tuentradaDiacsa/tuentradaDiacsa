473ab979ad486f44a458c2321c304f68838388e8
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-12 04:33:52 -0500

    Update views.py Apuntar bien al tipo de entrada

--- homePage/views.py
+++ homePage/views.py
@@ -399,7 +399,7 @@ class comprarPage(View):
                     if(i==5): k = 3
 
                     for j in range(int(request.POST.get("cantidadentrada"+str(i+1)))):
-                        auxiliarDisminuyeEntradas = Tipos.objects.get(id=k)
+                        auxiliarDisminuyeEntradas = Tipos.objects.get(id=k+1)
                         auxiliarDisminuyeEntradas.cantidad = auxiliarDisminuyeEntradas.cantidad-1
                         auxiliarDisminuyeEntradas.save()
                         
