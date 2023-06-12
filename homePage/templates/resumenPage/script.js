533a4ebbe86cd42be4177e5c14296afcd3aae03f
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-12 15:35:05 -0500

    Add files via upload

--- homePage/templates/resumenPage/script.js
+++ homePage/templates/resumenPage/script.js
@@ -21,7 +21,21 @@ function miFuncion() {
 
 }
 setInterval(miFuncion, 500);
+function copiarAlPortapapeles(texto) {
+    var elementoTemporal = document.createElement('textarea');
+    elementoTemporal.value = texto;
 
+    // Asegurarse de que el elemento esté fuera de la vista
+    elementoTemporal.style.position = 'absolute';
+    elementoTemporal.style.left = '-9999px';
+
+    document.body.appendChild(elementoTemporal);
+    elementoTemporal.select();
+    document.execCommand('copy');
+    document.body.removeChild(elementoTemporal);
+
+    alert('Texto copiado al portapapeles: ' + texto);
+}
 
 function confirmar() {
     document.getElementById("boton").value = "confirmarCompra"
