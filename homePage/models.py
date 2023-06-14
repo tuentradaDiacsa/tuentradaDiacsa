85ec8f58f586ef456bfb16e4542d4b922fc2b5ee
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-14 09:01:56 -0500

    Add files via upload

--- homePage/models.py
+++ homePage/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Pagos(models.Model):
     # key = models.ForeignKey()
     fechaHora = models.DateTimeField()  # Actual
-    celular   = models.TextField()
-    whatsapp  = models.TextField(blank=True, null=True)
+    celular = models.TextField()
+    whatsapp = models.TextField(blank=True, null=True)
     cip = models.TextField()  # Aleatorio 6 digitos
     pin = models.TextField(blank=True, null=True)
     monto = models.TextField()
@@ -19,7 +19,7 @@ class Pagos(models.Model):
     pregunta1 = models.TextField(max_length=50, blank=True, null=True)
     pregunta2 = models.TextField(max_length=50, blank=True, null=True)
     pregunta3 = models.TextField(max_length=50, blank=True, null=True)
-    
+
     def __str__(self):
         return str(self.cip)+" "+str(self.celular)+" "+str(self.monto)
 
@@ -36,18 +36,18 @@ class Tickets(models.Model):
         blank=True, null=True)  # Fecha del ultimo intento fallido
     # Fecha generacion igual a la del pin cambio igual a generacion pero con reinicio si se transfiere
     fechaHoraCambio = models.DateTimeField(blank=True, null=True)
-    celular         = models.TextField()
-    whatsapp        = models.TextField(blank=True, null=True)
+    celular = models.TextField()
+    whatsapp = models.TextField(blank=True, null=True)
 
-    tipo        = models.TextField()
-    numeroBox   = models.TextField(max_length=5, default='0')
-    cip         = models.TextField()  # antes recibo
+    tipo = models.TextField()
+    numeroBox = models.TextField(max_length=5, default='0')
+    cip = models.TextField()  # antes recibo
     confirmado2 = models.DateTimeField(
         blank=True, null=True)  # Por defecto blanco
 
-    nombre    = models.TextField(max_length=40, blank=True, null=True)
-    correo    = models.TextField(max_length=50, blank=True, null=True)
-    dni       = models.TextField(blank=True, null=True)
+    nombre = models.TextField(max_length=40, blank=True, null=True)
+    correo = models.TextField(max_length=50, blank=True, null=True)
+    dni = models.TextField(blank=True, null=True)
     pregunta1 = models.TextField(max_length=50, blank=True, null=True)
     pregunta2 = models.TextField(max_length=50, blank=True, null=True)
 
@@ -59,7 +59,8 @@ class Tickets(models.Model):
 
     def __str__(self):
         return str(self.cip)+" "+str(self.tipo)+" "+str(self.ticket)
-    
+
+
 class Tipos(models.Model):
     tipo = models.PositiveIntegerField()
     descripcion = models.TextField()
@@ -68,6 +69,7 @@ class Tipos(models.Model):
 
     def __str__(self):
         return self.descripcion+" "+str(self.cantidad)
+
 
 class Preguntas(models.Model):
     tipo = models.PositiveIntegerField()
@@ -82,7 +84,8 @@ class smsValidacionCelular(models.Model):
     celular = models.CharField(max_length=9)
     codigoValidacion = models.CharField(max_length=6)
     # Lo llena el script en la computadora
-    fechaHoraEnvio = models.DateTimeField(blank=True, null=True)
+    fechaSolicitud = models.DateTimeField(blank=True, null=True)
+    fechaEnvio = models.DateTimeField(blank=True, null=True)
     # Lo llena el script de la computadora
     estado = models.PositiveIntegerField(default=0)
 
@@ -93,22 +96,25 @@ class smsValidacionCelular(models.Model):
     def __str__(self):
         return str(self.correlativo)+" "+str(self.celular)+" "+str(self.codigoValidacion)+" "+str(self.estado)
 
+
 class boxesRestante1(models.Model):
-    box     = models.CharField(max_length=5)
+    box = models.CharField(max_length=5)
     ocupado = models.BooleanField(default=False)
 
     def __str__(self):
         return "box"+str(self.box)+" "+str("Ocupado" if self.ocupado else "Libre")
+
 
 class boxesRestante2(models.Model):
-    box     = models.CharField(max_length=5)
+    box = models.CharField(max_length=5)
     ocupado = models.BooleanField(default=False)
 
     def __str__(self):
         return "box"+str(self.box)+" "+str("Ocupado" if self.ocupado else "Libre")
 
+
 class boxesRestante3(models.Model):
-    box     = models.CharField(max_length=5)
+    box = models.CharField(max_length=5)
     ocupado = models.BooleanField(default=False)
 
     def __str__(self):
