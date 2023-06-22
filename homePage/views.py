9c76abd4fc6c8e7921f187dd04b7aad7dd63416a
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-22 17:23:01 -0500

    Add files via upload

--- homePage/views.py
+++ homePage/views.py
@@ -776,12 +776,12 @@ class comprarPage(View):
                 responseData = {"estado": "Confirmado"}
                 return JsonResponse(responseData)
 
-        elif request.POST.get("comando") == "cancelarCompra":
-                print("POST cancelarCompra")
-                cipRecibo = request.POST.get("cip")
-                devolverEntradas(cipRecibo)
-                responseData = {"estado": "Cancelado"}
-                return JsonResponse(responseData)
+            elif request.POST.get("comando") == "cancelarCompra":
+                    print("POST cancelarCompra")
+                    cipRecibo = request.POST.get("cip")
+                    devolverEntradas(cipRecibo)
+                    responseData = {"estado": "Cancelado"}
+                    return JsonResponse(responseData)
 
         return render(request, "comprarPage/comprarPage.html", datos)
 
