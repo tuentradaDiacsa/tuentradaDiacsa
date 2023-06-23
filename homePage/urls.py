4fff10ce9b38d9a5ed04afb468e9761d04dc3353
tuentradaDiacsa <136247981+tuentradaDiacsa@users.noreply.github.com>
2023-06-23 08:59:20 -0500

    Update urls.py

--- homePage/urls.py
+++ homePage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 from .views import comprarPage
+from .views import loaderio_verification
 #from django.conf.urls import url
 
 urlpatterns = [
@@ -14,6 +15,9 @@ urlpatterns = [
     path("escaner/", views.escanerPage, name="escanerPage"),
     path('descargar_boletoQREncriptado/<entrada_id>/', views.descargar_boletoQREncriptado, name='descargar_boletoQREncriptado'),
     path('qr', views.descargarQRPage, name='descargarQRPage'),
+    path('loaderio-068c49c7628e466db6843e532472ccfa.txt', loaderio_verification),
+    path('loaderio-068c49c7628e466db6843e532472ccfa.html', loaderio_verification),
+    path('loaderio-068c49c7628e466db6843e532472ccfa/', loaderio_verification),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
